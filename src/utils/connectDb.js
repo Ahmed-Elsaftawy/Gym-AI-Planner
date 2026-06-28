29150717e22d4e3b374017ed099ba9f9c5ae668c
Ahmed <gamerahmed933@gmail.com>
2026-06-29 02:32:46 +0300

    auto Create tables

--- src/utils/connectDb.js
+++ src/utils/connectDb.js
@@ -1,13 +1,9 @@
 import { Pool } from "pg";
 import { config } from "dotenv";
-import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
+import initDatabase from "../data/createTable";
 
 config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -21,32 +17,16 @@ const pool = new Pool({
     }
 });
 
-const initDatabase = async () => {
-    try {
-        const sqlPath = path.join(__dirname, '../data/init.sql');
-        const sql = await fs.readFile(sqlPath, 'utf8');
 
-        await pool.query(sql);
-        console.log('Database tables initialized successfully.');
-    } catch (err) {
-        console.error('Error initializing database tables:', err);
-    }
-};
 
 const connectDb = async () => {
-    try {
-        await pool.query('SELECT NOW()');
-        console.log(' Database is connected and responding.');
+    await initDatabase(pool);
+    await pool.connect()
+    pool.on('connect', () => {
+        console.log('database is connected');
 
-        await initDatabase();
-    } catch (err) {
-        console.error(' Database connection failed:', err);
-    }
-};
+    })
+}
 
-pool.on('connect', () => {
-    console.log('New client connected to the pool');
-});
-
-export default connectDb;
-export { pool };
+export default connectDb
+export { pool }
